4d13411a111bfe76322cc544dd92a734c2f33dac
racka98 <bmkilaha@gmail.com>
2020-11-24 09:43:27 +0300

    Some changes to Utilities folder

--- Utilities/alc_fix/uninstall.sh
+++ Utilities/alc_fix/uninstall.sh
@@ -2,18 +2,11 @@
 
 echo "Uninstalling ALCPlugFix.  Root user is required."
 
-# check if the root filesystem is writeable (starting with macOS 10.15 Catalina, the root filesystem is read-only by default)
-if sudo test ! -w "/"; then
-    echo "Root filesystem is not writeable.  Remounting as read-write and restarting Finder."
-    sudo mount -uw /
-    sudo killall Finder
-fi
-
-sudo rm /usr/bin/ALCPlugFix
-sudo rm /usr/bin/hda-verb
-sudo launchctl unload -w /Library/LaunchDaemons/com.echo.ALCPlugFix.plist
-sudo launchctl remove com.echo.ALCPlugFix
-sudo rm /Library/LaunchDaemons/com.echo.ALCPlugFix.plist
+sudo rm /usr/local/bin/ALCPlugFix
+sudo rm /usr/local/bin/hda-verb
+sudo launchctl unload -w /Library/LaunchDaemons/good.win.ALCPlugFix.plist
+sudo launchctl remove good.win.ALCPlugFix
+sudo rm /Library/LaunchDaemons/good.win.ALCPlugFix.plist
 
 echo "Done!"
 exit 0
